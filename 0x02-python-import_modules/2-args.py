b96b4b4dd43720b302f453c1aeb78f559b798831
Jotham Ntekim <jothamntekim@gmail.com>
2022-08-25 23:50:52 +0000

    added a readme file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-import sys
-
-
-def arguments():
-    """Prints number and lists of arguments"""
-
-    argc = len(argv)
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
     argc = argc - 1
-
     if argc == 0:
         print("{} arguments.".format(argc))
     elif argc == 1:
@@ -17,6 +12,3 @@ def arguments():
         print("{} arguments:".format(argc), end="\n")
         for i in range(int(argc)):
             print("{}: {}".format(i + 1, sys.argv[i + 1]), end="\n")
-
-
-arguments()

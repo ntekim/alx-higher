c430906d5396ecb91ac6184f40acafdd9466b781
Jotham Ntekim <jothamntekim@gmail.com>
2022-09-14 09:27:01 +0000

    module documented

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+"""Define a class Square"""
+
 
 class Square:
-    """Represents a square.
-    Attributes:
-        size (int): The size of the new square.
-    """
+    """Represents a square."""
 
     def __init__(self, size=0):
         """Initializes the data.
